5b2bf9ca4b98afe646e74e51a5d683acd457e8ab
admin <admin@192.168.181.1>
2023-01-19 12:22:17 +0530

    SampleTest2

---
+++ src/test/java/maven_project/sample_Test2.java
@@ -0,0 +1,13 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class sample_Test2 {
+	
+	@Test
+	public void SampleTest2()
+	{
+		System.out.println("--sampleTest2---");
+	}
+
+}
